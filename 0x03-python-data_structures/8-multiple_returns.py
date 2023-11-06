3437fa6655fa4eb26b5f0473d12f22a3b5ec0c28
Okechukwu Kingsley <kingsleyokechukwu890@gmail.com>
2023-11-06 13:51:32 -0800

    project: 0x03. python - data structures

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if not sentenc:
+        sentence = None
+        if sentence:
+            sen_len = len(sentence)
+        else:
+            sen_len = 0
+            return (sen_len, sentence if not sentence else sentence[:1])
