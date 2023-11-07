6c8d3284a9ccd29f0a0564820e198c7987d91c2a
Okechukwu Kingsley <kingsleyokechukwu890@gmail.com>
2023-11-07 13:48:06 -0800

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 def no_c(my_string):
-    new_string = ""
+    new_str = ''
     for char in my_string:
-        if char not in ['c', 'C']:elements != "c" and elements != "C":
-            new_string += char
-            return new_string
+        if char != "c" and char != "C":
+            new_str += char
+           return (new_str)
