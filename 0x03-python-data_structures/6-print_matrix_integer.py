3b969cd53b1183d3f0bc814ab4df6d9673ba9e74
Okechukwu Kingsley <kingsleyokechukwu890@gmail.com>
2023-11-07 20:56:18 -0800

    project: 0x03.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end="")
-            if j != (len(matrix[i]) - 1):
-                print(" ", end="")
-
-                print("")
+    for row in matrix:
+        for i in range(len(row)):
+            if i == 0:
+                print("{:d}".format(row[i]), end="")
+            else:
+                print(" {:d}".format(row[i]), end="")
+                print()
