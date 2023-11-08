e2f9f0cfab419bdd2def47cd6a361785898a200d
Okechukwu Kingsley <kingsleyokechukwu890@gmail.com>
2023-11-08 13:11:03 -0800

    correct 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-def multiply_by_2(a_dictionary):
-    new_dic = {}
-    for i in a_dictionary:
-        new_dic[i] = a_dictionary[i] * 2
-        return new_dic
+def multiply_by_2(my_dict):
+    return {key: val*2 for key, val in my_dict.items()}
