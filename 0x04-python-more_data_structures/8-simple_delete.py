eb7b61090d571880315cbc3b82f60898a242eb69
Okechukwu Kingsley <kingsleyokechukwu890@gmail.com>
2023-11-08 19:52:45 -0800

    8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
-def simple_delete(my_dict, key=""):
-    my_dict.pop(key, None)
-    return my_dict    
+def simple_delete(a_dictionary, key=""):
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
+
+# Example usage
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+key_to_delete = 'b'
+result_dict = simple_delete(my_dict, key_to_delete)
+print(result_dict)
