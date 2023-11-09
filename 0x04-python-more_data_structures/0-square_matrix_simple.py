7e8196f87f2c9b58596e71980cddd88ebe9881e4
Okechukwu Kingsley <kingsleyokechukwu890@gmail.com>
2023-11-08 19:22:52 -0800

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+
 def square_matrix_simple(matrix=[]):
-    squared = []
-    for line in matrix:
-        squared.append([c**2 for c in line])
-        return squared
+    if not matrix:
+        return []
+
+    result_matrix = [[0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            result_matrix[i][j] = matrix[i][j] ** 2
+
+            return result_matrix
+
+# Example usage
+input_matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+output_matrix = square_matrix_simple(input_matrix)
+print(output_matrix)
