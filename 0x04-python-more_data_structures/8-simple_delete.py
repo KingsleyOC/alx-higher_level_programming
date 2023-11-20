66e7614372a8bde0cb992b749c7582743b6e8a69
Okechukwu Kingsley <kingsleyokechukwu890@gmail.com>
2023-11-20 14:11:13 -0800

    0x05. python-exception

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
+simple_delete = __import__('8-simple_delete').simple_delete
+print_sorted_dictionary = \
+        __import__('6-print_sorted_dictionay').print_sorted_dictionary
 
-def simple_delete(a_dictionary, key=""):
-    if key in a_dictionary:
-        del a_dictionary[key]
-    return a_dictionary
+a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3] }
+new_dict = simple_delete(a_dictionary, 'track')
+print_sorted_dictionary(a_dictionary)
+print("--")
+print_sorted_dictionary(new_dict)
 
-# Example usage
-my_dict = {'a': 1, 'b': 2, 'c': 3}
-key_to_delete = 'b'
-result_dict = simple_delete(my_dict, key_to_delete)
-print(result_dict)
+print("--")
+print("--")
+new_dict = simple_delete(a_dictionary, 'c_is_fun')
+print_sorted_dictionary(a_dictionary)
+print("--")
+print_sorted_dictionary(new_dict)
