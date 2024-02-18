bd82e59c3aa6461046f0a41c8459fc18b237cbc4
KingsleyOC <kingsleyokechukwu890@gmail.com>
2024-02-18 15:58:17 -0800

    relational_mapping

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
-from relationship_state import Base, State
-from relationship_city import City
-from sqlalchemy.orm import sessionmaker, relationship
-from sqlalchemy import create_engine
-import sys
-"""
-    Module that performs MySQL query through MySQLAlchemy.
-"""
+"""Script that lists all City objects from the database hbtn_0e_101_usa"""
 
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import Session
+from relationship_city import Base, City, State
+import sys
 
 if __name__ == "__main__":
-    db_uri = 'mysql+mysqldb://{}:{}@localhost/{}'.format(
-                                                            sys.argv[1],
-                                                            sys.argv[2],
-                                                            sys.argv[3])
+    # Connect to MySQL server using SQLAlchemy
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
-    engine = create_engine(db_uri, pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    # Create a session to interact with the database
+    session = Session(engine)
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    # Query to retrieve City objects and their associated State objects
+    query = text("SELECT cities.id, cities.name, states.name FROM cities "
+                 "JOIN states ON cities.state_id = states.id "
+                 "ORDER BY cities.id")
 
-    for a_city in session.query(City).order_by(City.id):
-            print("{}: {} -> {}".format(a_city.id, a_city.name,
-                  a_city.state.name))
+    # Execute the query
+    result = session.execute(query)
 
+    # Fetch all the rows and print the results
+    for row in result.fetchall():
+        print("{}: {} -> {}".format(row[0], row[1], row[2]))
+
+    # Close the session
     session.close()
